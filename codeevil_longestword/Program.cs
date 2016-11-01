7be92859af6c983de8eb6fffdb9c1bb8a9b20804
ZerrickStaples <zerrick.staples@gmail.com>
2016-11-01 16:23:25 -0400

    Add files via upload

---
+++ codeevil_longestword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeevil_longestword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a sentence.");
+            string sentence = Console.ReadLine();
+            string[] sentenceArray = sentence.Split();
+            string longestWord = "";
+            int mostLetters = 0;
+            foreach (string word in sentenceArray)
+            {
+                if(word.Length >= mostLetters)
+                {
+                    longestWord = word;
+                    mostLetters = word.Length;
+                }
+
+            }
+            Console.WriteLine(longestWord);
+        }
+    }
+}
+    
